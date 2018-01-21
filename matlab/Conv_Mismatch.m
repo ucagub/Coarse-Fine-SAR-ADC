7e86f11100d74122c7c33e41915c5953991c9d96
rcmonsayac <35493715+rcmonsayac@users.noreply.github.com>
2018-01-21 16:16:45 +0800

    adfsaf
    fafas

--- matlab/Conv_Mismatch.m
+++ matlab/Conv_Mismatch.m
@@ -1,9 +1,10 @@
 Vout = zeros(1,256);
-Cu = 1e-12;
-sigma_Cu = Cu*.01;
+Cap = 1e-15;
+Cu = 1*Cap;
+sigma_Cu = 0.02;
 LSB = 1/2^8;
 Carray = Cu.*[2^7 2^6 2^5 2^4 2^3 2^2 2^1 2^0];
-Ctot = 256e-12;
+Ctot = 256*Cap;
 for Vin = 0:255
     
         Din = decimalToBinaryVector(Vin,8,'MSBFirst');
@@ -14,16 +15,19 @@ for Vin = 0:255
                   for k = 1:j
                       Cup_tot = Cup_tot + Carray(Cup(k));
                   end
-                  Cup_tot_sigma = sqrt(Cup_tot)*Cu*sigma_Cu;
+                 
+                  Cup_tot_sigma = sqrt(Cup_tot/Cap)*Cu*sigma_Cu;
+                  Cup_tot
+                  Cup_tot_sigma
                   R = normrnd(Cup_tot,Cup_tot_sigma);
-        Vout(Vin+1) = R/Ctot;
+        Vout(Vin+1) = R/normrnd(Ctot,sqrt(Ctot/Cap)*Cu*sigma_Cu);
   
 end
 DNL = zeros(1,256);
 output_code = [0:1:255];
 for u = 1:255
-   DNL(u) = (Vout(u+1)-Vout(u)) - LSB;
+   DNL(u) = ((Vout(u+1)-Vout(u)) - LSB)/LSB;
 end
-DNL(256) = (1-Vout(256)) - LSB;
+DNL(256) = ((1-Vout(256)) - LSB)/LSB;
 
 plot(output_code,DNL)
