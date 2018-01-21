8ce7095f0afa862970ade4f2d14867591d8fb628
rcmonsayac <35493715+rcmonsayac@users.noreply.github.com>
2018-01-21 22:05:33 +0800

    ddada
    dadadas

--- matlab/Conv_Mismatch.m
+++ matlab/Conv_Mismatch.m
@@ -1,33 +1,57 @@
-Vout = zeros(1,256);
-Cap = 1e-15;
-Cu = 1*Cap;
-sigma_Cu = 0.02;
-LSB = 1/2^8;
-Carray = Cu.*[2^7 2^6 2^5 2^4 2^3 2^2 2^1 2^0];
-Ctot = 256*Cap;
-for Vin = 0:255
-    
-        Din = decimalToBinaryVector(Vin,8,'MSBFirst');
-        Cup_tot = 0;
-        Cup = find(Din);
-                  i = size(Cup);
-                  j = i(2);
-                  for k = 1:j
-                      Cup_tot = Cup_tot + Carray(Cup(k));
-                  end
-                 
-                  Cup_tot_sigma = sqrt(Cup_tot/Cap)*Cu*sigma_Cu;
-                  Cup_tot
-                  Cup_tot_sigma
-                  R = normrnd(Cup_tot,Cup_tot_sigma);
-        Vout(Vin+1) = R/normrnd(Ctot,sqrt(Ctot/Cap)*Cu*sigma_Cu);
-  
-end
-DNL = zeros(1,256);
-output_code = [0:1:255];
-for u = 1:255
-   DNL(u) = ((Vout(u+1)-Vout(u)) - LSB)/LSB;
-end
-DNL(256) = ((1-Vout(256)) - LSB)/LSB;
 
-plot(output_code,DNL)
+
+DNLarray = [];
+INLarray = [];
+for x = 1:1000
+    Vout = zeros(1,256);
+    Cap = 1e-15;
+    Cu = 1*Cap;
+    sigma_Cu = 0.05;
+    LSB = 1/2^8;
+    Cmm = zeros(1,8);
+
+    Carray = Cu.*[2^0 2^1 2^2 2^3 2^4 2^5 2^6 2^7];
+    for a = 1:8
+        sigma = Carray(a)*sigma_Cu/sqrt(2^(a-1));
+        Cmm(a) = normrnd(Carray(a),sigma);
+    end
+    Ctot = sum(Cmm) + normrnd(Cu,Cu*sigma_Cu);
+    for Vin = 0:255
+
+            Din = decimalToBinaryVector(Vin,8,'LSBFirst');
+            Cup_tot = 0;
+            Cup = find(Din);
+                      i = size(Cup);
+                      j = i(2);
+                      for k = 1:j
+                          Cup_tot = Cup_tot + Cmm(Cup(k));
+                      end
+
+            Vout(Vin+1) = Cup_tot/Ctot;
+
+    end
+    DNL = zeros(1,256);
+    INL = zeros(1,256);
+    output_code = [0:1:255];
+    for u = 1:255
+       DNL(u) = ((Vout(u+1)-Vout(u)) - LSB)/LSB;
+    end
+    DNL(256) = ((1-Vout(256)) - LSB)/LSB;
+    %figure;
+    %stem(output_code,DNL)
+    for u = 1:255
+        for v = 1:u
+            INL(u) = INL(u) + DNL(v);
+        end
+    end
+    %figure;
+   % stem(output_code,INL)
+   % figure;
+    %plot(Vout)
+    DNLarray = [DNLarray;DNL];
+end
+
+for y = 1:256
+   stddev(y) = sqrt(var(DNLarray(:,y)));
+end
+
