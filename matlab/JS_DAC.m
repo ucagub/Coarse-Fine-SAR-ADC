41368f91c3bc9df561a49590d94198d625c78d16
ucagub <ucagub@gmail.com>
2018-01-27 07:24:36 +0800

    JS

--- matlab/JS_DAC.m
+++ matlab/JS_DAC.m
@@ -8,6 +8,7 @@ classdef JS_DAC
         Vouts
         DNL
         INL
+        Epercycle
         %abs_max_DNL
         %DNL_stdev
         %value
@@ -31,6 +32,7 @@ classdef JS_DAC
                           16 8 4 2 2 0 0; 
                           32 16 8 4 2 2 0; 
                           64 32 16 8 4 2 2];
+            
             obj.Ctup = add_mismatch(obj.Ctup);
             obj.Ctdown = add_mismatch(obj.Ctdown);
             
@@ -42,9 +44,9 @@ classdef JS_DAC
             obj.Vouts = get_Vouts(obj);
             obj.DNL = get_DNL(obj);
             obj.INL = get_INL(obj);
+            obj.Epercycle = get_Epercycle(obj);
             %obj.abs_max_DNL = max(abs(obj.DNL));
             %obj.DNL_stdev = sqrt(var(obj.DNL));
-            %get_Energy_code(obj, 128)
         end
 
         function y = eval(obj, Vin)
@@ -56,6 +58,30 @@ classdef JS_DAC
                 Vout = obj.Vouts(Vin);
             end
             y = Vout;
+        end
+        
+
+    end
+    methods(Access = private)
+        function init_mismatch(obj)
+            obj.Ctdown = 2;
+            %Cap = 1e-6;
+            Cap = 1;
+            Cu = 1*Cap;
+            sigma_Cu = 0.05;
+
+            sigma = Cu*obj.Ctup*sigma_Cu/sqrt(obj.Ctup);
+            obj.Ctup = normrnd(Cu*obj.Ctup,sigma);
+
+            sigma = Cu*obj.Ctdown*sigma_Cu/sqrt(obj.Ctdown);
+            obj.Ctdown = normrnd(Cu*obj.Ctdown,sigma);
+
+            for a = 1:7
+                for b = 1:7
+                    sigma = Cu*obj.Carray(a,b)*sigma_Cu/sqrt(obj.Carray(a,b));
+                    obj.Carray(a,b) = normrnd(Cu*obj.Carray(a,b),sigma);
+                end
+            end
         end
     end
 end
@@ -88,48 +114,58 @@ function y = add_mismatch(u)
     buff = normrnd(Cu*buff,sigma);
     y = buff;
 end
-
-function y = get_Energy_code(obj, Vin)
-    
+function y = get_Epercycle(obj)
+    Epercycle = zeros(1,255);
+    for i = 1:255
+        Epercycle(i) = get_Ecycle1(obj, i);
+    end
+    y = Epercycle(i);
+end
+function Ecycle = get_Ecycle1(obj, Vin)
+    Varray_init = zeros(7,7);
     Etotal = 0;
     Vref = 1;
+    codeCycle = get_codeCycle(Vin);
+    %get Vf given code
+    
+    for i = 2:8
+        Varray_final = get_Varray(obj, codeCycle(i));
+        %Varray_final = get_Varray(obj, i);
+        CV_array = obj.Carray.*(Varray_final-Varray_init);
+        Etran = -Vref*sum(sum(CV_array(1:i-1, 1:i-1)));
+        Etotal = Etotal + Etran; 
+        Varray_init = Varray_final;
+    end
+    Ecycle = Etotal;
+end
+
+function Varray_final = get_Varray(obj, code)
+    Vref = 1;
+    Varray_buff = zeros(7,7);
+    config = de2bi(code,8,'left-msb');
+    Vx = obj.eval(code);
+    
+    V1d = (Vx - Vref*config);
+    for i = 1:7
+        Varray_buff(:,i) = V1d(i);
+    end
+    Varray_final = Varray_buff;
+    
+end
+
+function codeCycle = get_codeCycle(Vin)
     Vup = 256;
     Vdown = 0;
-    code = zeros(1,7);
-    if Vin >= (Vup+Vdown)/2
-       Vdown = (Vup+Vdown)/2;       
-       code(1) = 1;
-    else 
-       Vup = (Vup+Vdown)/2;
-       code(1) = 0;
-    end
-        
-    Vi = zeros(7,7);
-    Vf = zeros(7,7);
+    
+    code_buff = [128];
     for i = 1:7
         if Vin >= (Vup+Vdown)/2
            Vdown = (Vup+Vdown)/2;
-           code(i) = 1; 
-        else 
+           code_buff = [code_buff (Vup+Vdown)/2];
+        else
            Vup = (Vup+Vdown)/2;
-           code(i) = 0; 
+           code_buff = [code_buff (Vup+Vdown)/2];
         end 
-        Vx = obj.eval((Vup+Vdown)/2);
-        for k = 1:i
-            for l = 1:i
-                if code(l) == 1
-                    Vf(k,l) = Vx - Vref;
-                else
-                    Vf(k,l) = Vx;
-                end
-            end
-        end
-       
-        Ecycle = -Vref*sum(sum((obj.Carray.*(Vf-Vi))));
-        Etotal = Etotal + Ecycle;
-
-        Vi = Vf;
     end
-    Etotal
-    y = Etotal;
+    codeCycle = code_buff;
 end
