7761ed9f05e4954f6c47ba80e59bc0b7d9a9fb59
ucagub <ucagub@gmail.com>
2018-01-26 15:28:08 +0800

    JS_DAC scrappy with errors in energy

--- matlab/JS_DAC.m
+++ matlab/JS_DAC.m
@@ -8,6 +8,7 @@ classdef JS_DAC
         Vouts
         DNL
         INL
+        Epercycle
         %abs_max_DNL
         %DNL_stdev
         %value
@@ -43,6 +44,7 @@ classdef JS_DAC
             obj.Vouts = get_Vouts(obj);
             obj.DNL = get_DNL(obj);
             obj.INL = get_INL(obj);
+            obj.Epercycle = get_Epercycle(obj);
             %obj.abs_max_DNL = max(abs(obj.DNL));
             %obj.DNL_stdev = sqrt(var(obj.DNL));
         end
@@ -93,7 +95,7 @@ function y = get_Vouts(obj)
         for j = 1:2^(i-1)
             code = de2bi(j-1,i-1,'left-msb');
             Cup = sum(obj.Carray(1:i-1,1:i-1)*code') + obj.Ctup;
-            Ctot = sum(sum(obj.Carray(1:i-1,1:i-1))) + (obj.Ctup + obj.Ctdown)
+            Ctot = sum(sum(obj.Carray(1:i-1,1:i-1))) + (obj.Ctup + obj.Ctdown);
             Vout = [Vout Cup/Ctot];
         end
     end
@@ -112,8 +114,14 @@ function y = add_mismatch(u)
     buff = normrnd(Cu*buff,sigma);
     y = buff;
 end
-
-function Ecycle = get_Ecycle(obj, Vin)
+function y = get_Epercycle(obj)
+    Epercycle = zeros(1,255);
+    for i = 1:255
+        Epercycle(i) = get_Ecycle1(obj, i);
+    end
+    y = Epercycle(i);
+end
+function Ecycle = get_Ecycle1(obj, Vin)
     Varray_init = zeros(7,7);
     Etotal = 0;
     Vref = 1;
@@ -121,30 +129,42 @@ function Ecycle = get_Ecycle(obj, Vin)
     %get Vf given code
     
     for i = 2:8
-        Varray_final = get_Varray(codeCycle(i)); 
-        Etran = -Vref*sum(sum(obj.Carray.*(Varray_final-Varray_init)));
+        Varray_final = get_Varray(obj, codeCycle(i));
+        CV_array = obj.Carray.*(Varray_final-Varray_init);
+        Etran = -Vref*sum(sum(CV_array(1:i-1, 1:i-1)));
         Etotal = Etotal + Etran; 
+        Varray_init = Varray_final;
     end
     Ecycle = Etotal;
 end
 
-function Varray_final = get_Varray(code)
-
+function Varray_final = get_Varray(obj, code)
+    Vref = 1;
+    Varray_buff = zeros(7,7);
+    config = de2bi(8,8,'left-msb');
+    Vx = obj.eval(code);
+    V1d = (Vx - Vref*config);
+    for i = 1:7
+        Varray_buff(:,i) = V1d(i);
+    end
+    Varray_final = Varray_buff;
+    
 end
 
 function codeCycle = get_codeCycle(Vin)
     Vup = 256;
     Vdown = 0;
     
-    code_Cycle = [];
+    code_buff = [];
     for i = 1:8
         if Vin >= (Vup+Vdown)/2
            Vdown = (Vup+Vdown)/2;
-           codeCycle = [codeCycle (Vup+Vdown)/2];
+           code_buff = [code_buff (Vup+Vdown)/2];
         else
            Vup = (Vup+Vdown)/2;
-           codeCycle = [codeCycle (Vup+Vdown)/2];
+           code_buff = [code_buff (Vup+Vdown)/2];
         end 
     end
+    codeCycle = code_buff;
 end
 
